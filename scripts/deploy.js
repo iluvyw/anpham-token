8874ef9f6188c5eef9f01ecd2bdb65b5016765bc
An Pham <anhoangpham2001@gmail.com>
2022-09-27 01:10:21 +0700

    Total supply update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,7 @@ const { BigNumber } = require("ethers");
 async function main() {
   const [deployer] = await ethers.getSigners();
 
-  const totalSupply = BigNumber.from(1000 * 10 ** 10);
+  const totalSupply = BigNumber.from(2408 * 10 ** 10);
 
   console.log("Deploying contracts with the account:", deployer.address);
 
